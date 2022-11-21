c97736ec7ca5cb3fdd68bc4bd3dfeff7cdae0df3
Alexey Gorbunov <alexeygorbunov323@gmail.com>
2022-11-21 19:43:16 +0300

    Добавил отправку токена

--- Targets/Job4m2/Sources/AppDelegate.swift
+++ Targets/Job4m2/Sources/AppDelegate.swift
@@ -2,19 +2,58 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var appCoordinator: AppCoordinator?
-
+    
+    let apiService = APIService()
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
-       
+        
         let appCoordinator = AppCoordinator()
         self.appCoordinator = appCoordinator
         appCoordinator.start()
-
+        
         return true
     }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        
+        Task {
+            let a: A? = await apiService.perform(
+                route: APIRoute(route: Route.User.apns.asPath, method: .post),
+                parameters: AnusDTO(token: token)
+            )
+        }
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        print("Пизда")
+        if let scheme = url.scheme,
+           scheme.localizedCaseInsensitiveCompare("job4mscheme") == .orderedSame,
+           let view = url.host {
+            
+            var parameters: [String: String] = [:]
+            URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.forEach {
+                parameters[$0.name] = $0.value
+            }
+            print(parameters)
+            
+        }
+        return true
+    }
+}
 
+struct A: Decodable { }
+struct AnusDTO: Encodable {
+    let token: String
 }
--- Targets/Job4m2/Sources/Registration/RegistrationCoordinator.swift
+++ Targets/Job4m2/Sources/Registration/RegistrationCoordinator.swift
@@ -29,6 +29,28 @@ final class RegistrationCoordinator {
         controller.onRegistrationFinish = {
             print("hui")
             self.onFinishEvent?()
+            self.registerForPushNotifications()
         }
+    }
+
+    func registerForPushNotifications() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            UNUserNotificationCenter.current()
+              .requestAuthorization(
+                options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+                self?.getNotificationSettings()
+            }
+        }
+    }
+
+    func getNotificationSettings() {
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+          guard settings.authorizationStatus == .authorized else { return }
+          DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+          }
+      }
     }
 }
